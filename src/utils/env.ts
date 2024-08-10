8c93ff7db6869e0d7b54e8bd2aafc2a91750dc62
yusufkamal-ok <59947329+yusufkamal-ok@users.noreply.github.com>
2024-08-10 16:44:01 +0700

    update

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,6 +6,6 @@ export const CLOUDINARY_API_SECRET: string =
   process.env.CLOUDINARY_API_SECRET || "";
 export const CLOUDINARY_CLOUD_NAME: string =
   process.env.CLOUDINARY_CLOUD_NAME || "";
-export const DATABASE_URL: string = ;
+export let DATABASE_URL: string 
 
 export const SECRET: string = process.env.SECRET || "";
